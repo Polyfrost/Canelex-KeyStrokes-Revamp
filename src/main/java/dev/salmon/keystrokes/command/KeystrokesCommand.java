60d4b6cdde01ba88f9b493695fa97ad2dbc9f6e4
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-10-25 13:49:01 -0400

    add alias

--- src/main/java/dev/salmon/keystrokes/command/KeystrokesCommand.java
+++ src/main/java/dev/salmon/keystrokes/command/KeystrokesCommand.java
@@ -4,7 +4,7 @@ import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
 import dev.salmon.keystrokes.Keystrokes;
 
-@Command(Keystrokes.ID)
+@Command(value = Keystrokes.ID, aliases = "keystrokes")
 public class KeystrokesCommand {
     @Main
     private void main() {
